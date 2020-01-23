cdba40aca85198135eb0e77c3a4bc2dd21ac0194
Rahul Natarajan <rahulnatarajan@s-164-67-205-45.resnet.ucla.edu>
2020-01-22 19:13:40 -0800

    remove metadata

--- recipe/exceptions/Exceptions.js
+++ recipe/exceptions/Exceptions.js
@@ -1,9 +1,66 @@
-class FirebaseError{
-
-    constructor(message){
-        throw new Error(message);
+class FirebaseAuthError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
     }
-
 }
 
-module.exports = { FirebaseError };
+class FirebaseLoginError extends FirebaseAuthError {
+    constructor(username, query) {
+        super(`Invalid username: ${username} and password.`);
+        this.data = { username, query };
+    }
+}
+
+class FirebaseSignupError extends FirebaseAuthError {
+    constructor(signupIssue, query) {
+        super(`Signup invalid: ${signupIssue}.`);
+        this.data = { signupIssue, query };
+    }
+}
+
+class ReadingError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+class TextReadError extends ReadingError {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+class CredentialsError extends ReadingError {
+    constructor(message) {
+        super(`Invalid GOOGLE_ACCOUNT_CREDENTIALS: ${message}`);
+    }
+}
+
+class WordError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+class ProcessingError extends WordError {
+    constructor(food, query) {
+        super(`Error processing food word: ${food}.`);
+        this.data = { food, query };
+    }
+}
+
+module.exports = { 
+    FirebaseLoginError, 
+    FirebaseSignupError,
+    TextReadError, 
+    CredentialsError, 
+    ProcessingError 
+};
